8fd75456ff389a6e75b8cb7304350bc6ce86fb1b
Tcodingg <princetlive@yahoo.com>
2022-02-14 13:53:31 -0500

    add redux action

---
+++ src/redux/actions/actions.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { actionTypes, Actions } from "./actionTypes";
+import {
+   topRatedUrl,
+   popularUrl,
+   nowPlayingUrl,
+   API_KEY,
+} from "../../tools/api/url";
+
+export const getMovies = () => async (dispatch: Dispatch<Actions>) => {
+   dispatch({ type: actionTypes.FETCH_MOVIE_LOADING });
+   try {
+      const topRated = await axios.get(topRatedUrl, {
+         params: {
+            api_key: API_KEY,
+         },
+      });
+
+      const popular = await axios.get(popularUrl, {
+         params: {
+            api_key: API_KEY,
+         },
+      });
+      const nowPlaying = await axios.get(nowPlayingUrl, {
+         params: {
+            api_key: API_KEY,
+         },
+      });
+      dispatch({
+         type: actionTypes.FETCH_MOVIE_SUCCESS,
+         payload: {
+            topRated: topRated.data.results,
+            popular: popular.data.results,
+            nowPlaying: nowPlaying.data.results,
+         },
+      });
+      // console.log(topRated);
+   } catch (err: any) {
+      dispatch({
+         type: actionTypes.FETCH_MOVIE_FAIL,
+         payload: err,
+      });
+   }
+};
--- src/tools/api/url.ts
+++ src/tools/api/url.ts
@@ -1,5 +1,12 @@
-export const API_KEY = "";
-export const topRatedUrl = "";
-
-export const popularUrl = "";
-export const nowPlayingUrl = "";
+export const topRatedUrl: string = process.env.REACT_APP_TOP_RATED_URL || "";
+export const popularUrl: string = process.env.REACT_APP_POPULAR_URL || "";
+export const nowPlayingUrl: string =
+   process.env.REACT_APP_NOW_PLAYING_URL || "";
+export const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+export const youtube_videos = process.env.REACT_APP_YOUTUBE_VIDEOS;
+export const youtube_link = process.env.REACT_APP_YOUTUBE_LINK;
+export const recommendedUrl = process.env.REACT_APP_RECOMMENDED_URL;
+export const movieDataUrl = process.env.REACT_APP_MOVIE_DATA_URL;
+export const poster_url = process.env.REACT_APP_POSTERS;
+export const search_movie_url = process.env.REACT_APP_SEARCH_MOVIES;
+export const youtube_url = process.env.REACT_APP_YOUTUBE;
