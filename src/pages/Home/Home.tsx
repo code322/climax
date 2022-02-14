7eb991e41e01a89776450d4ba4f0c4082c4490b2
Tcodingg <princetlive@yahoo.com>
2022-02-14 12:05:06 -0500

    style hero

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,22 +6,26 @@ function Home() {
    return (
       <section className="home">
          <div className="home-container ">
-            <div className="hero-container bd-container">
-               <input
-                  className="input "
-                  type="text"
-                  placeholder="Search Movie..."
-               />
+            <div className="hero-container">
+               <div className="input-container bd-container">
+                  <input
+                     className="input "
+                     type="text"
+                     placeholder="Search Movie..."
+                  />
+               </div>
                <div className="hero">
-                  <div className="hero-title">
-                     <h1>enjoy your tv</h1>
-                     <p>
-                        Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple
-                        TV, Blu-ray players, and more.
-                     </p>
-                  </div>
-                  <div className="img-container">
-                     <img src={tv} alt="Tv" />
+                  <div className="hero-wrapper bd-container">
+                     <div className="hero-info">
+                        <h1>enjoy your TV.</h1>
+                        <p>
+                           Watch on Smart TVs, Playstation, Xbox, Chromecast,
+                           Apple TV, Blu-ray players, and more.
+                        </p>
+                     </div>
+                     <div className="img-container">
+                        <img src={tv} alt="Tv" />
+                     </div>
                   </div>
                </div>
             </div>
