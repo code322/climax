4a9fcb88468bbe18e5b785fcd0c961d0936f4e9a
Tcodingg <princetlive@yahoo.com>
2022-02-14 17:08:33 -0500

    setup the routes and add home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Home from "./pages/Home/Home";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
    return (
-      <>
-         <Home />
-      </>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Home />} />
+         </Routes>
+      </BrowserRouter>
    );
 };
 
