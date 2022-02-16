8a7a5b699a0adb666aaf89492f843c124fb651c5
Tcodingg <princetlive@yahoo.com>
2022-02-16 13:40:02 -0500

    add css variables to bd-container

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Details.scss";
+import "../../index.scss";
 
 const Details: React.FC = () => {
-   return <div>Details</div>;
+   return (
+      <div className="details">
+         <div className="details-container bd-container"> details</div>
+      </div>
+   );
 };
 
 export default Details;
