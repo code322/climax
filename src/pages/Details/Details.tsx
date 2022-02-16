f418d00f827bb8fde2db2fb58551d48db4cc6a1b
Tcodingg <princetlive@yahoo.com>
2022-02-16 14:49:52 -0500

    add getdetails action types

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Details.scss";
 import "../../index.scss";
+import { useParams } from "react-router-dom";
 
 const Details: React.FC = () => {
+   const { id } = useParams();
+   console.log(id);
    return (
       <div className="details">
          <div className="details-container bd-container"> details</div>
--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -2,6 +2,11 @@ export enum actionTypes {
    FETCH_MOVIE_LOADING = "FETCH_MOVIE_LOADING",
    FETCH_MOVIE_SUCCESS = "FETCH_MOVIE_SUCCESS",
    FETCH_MOVIE_FAIL = "FETCH_MOVIE_FAIL",
+
+   //GET DETAILS
+   GET_DETAILS_LOADING = "GET_DETAILS_LOADING",
+   GET_DETAILS_SUCCESS = "GET_DETAILS_SUCCESS",
+   GET_DETAILS_FAIL = "GET_DETAILS_FAIL",
 }
 
 type actionLoad = {
@@ -26,5 +31,29 @@ export interface movieData {
    id: Number;
    poster_path: string;
 }
+
+// GET DETAILS
+type actionGetDetailsLoading = {
+   type: actionTypes.GET_DETAILS_LOADING;
+};
+
+type actionGetDetailsSuccess = {
+   type: actionTypes.GET_DETAILS_SUCCESS;
+   payload: {
+      description: {
+         title: string;
+         overview: string;
+         vote_average: number;
+         genres: [{ id: number; name: string }];
+      };
+      youtubeVideoKey: string;
+      recommended: movieData[];
+   };
+};
+
+type actionGetDetailsFail = {
+   type: actionTypes.GET_DETAILS_FAIL;
+   payload: string;
+};
 
 export type Actions = actionLoad | actionSuccess | actionFail;
