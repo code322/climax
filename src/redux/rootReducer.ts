c9c06d23b63ebf3b9821196d6b436b7164693ad1
Tcodingg <princetlive@yahoo.com>
2022-02-17 11:53:16 -0500

    add deialsReducer to root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,6 +1,8 @@
+import { detailsReducer } from "./reducers/getDetailsReducer";
 import { combineReducers } from "redux";
 import { movieReducer } from "./reducers/getMoviesReducer";
 export const rootReducer = combineReducers({
    movieReducer,
+   detailsReducer,
 });
 export type RootState = ReturnType<typeof rootReducer>;
