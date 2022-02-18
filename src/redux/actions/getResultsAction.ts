7d8731d047e05dba215dd964a1ad490e5cfe446e
Tcodingg <princetlive@yahoo.com>
2022-02-18 12:22:31 -0500

    complete getResult actions

--- src/redux/actions/getResultsAction.ts
+++ src/redux/actions/getResultsAction.ts
@@ -1,15 +1,32 @@
-import { API_KEY } from "./../../tools/api/url";
-import { Actions } from "./actionTypes";
+import { API_KEY } from "../../tools/api/url";
+import { Actions, actionTypes } from "./actionTypes";
 import axios from "axios";
 import { Dispatch } from "redux";
 import { search_movie_url } from "../../tools/api/url";
 
-const getResults =
+export const getResults =
    (movieName: string) => async (dispatch: Dispatch<Actions>) => {
-      const data = await axios.get(`${search_movie_url}`, {
-         params: {
-            api_Key: API_KEY,
-            query: movieName,
-         },
+      dispatch({
+         type: actionTypes.GET_RESULTS_LOADING,
       });
+
+      try {
+         const {
+            data: { results },
+         } = await axios.get(`${search_movie_url}`, {
+            params: {
+               api_Key: API_KEY,
+               query: movieName,
+            },
+         });
+         dispatch({
+            type: actionTypes.GET_RESULTS_SUCCESS,
+            payload: results,
+         });
+      } catch (error: any) {
+         dispatch({
+            type: actionTypes.GET_RESULTS_FAIL,
+            payload: error,
+         });
+      }
    };
