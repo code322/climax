952842571bab6c35f56387496fcc27a15b0c72b2
Tcodingg <princetlive@yahoo.com>
2022-02-18 12:34:55 -0500

    add results reducer into root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,8 +1,10 @@
+import { resultsReducer } from "./reducers/getResultReducer";
 import { detailsReducer } from "./reducers/getDetailsReducer";
 import { combineReducers } from "redux";
 import { movieReducer } from "./reducers/getMoviesReducer";
 export const rootReducer = combineReducers({
    movieReducer,
    detailsReducer,
+   resultsReducer,
 });
 export type RootState = ReturnType<typeof rootReducer>;
