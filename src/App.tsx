a221e28473bcbea90969ba200387b4a466389031
Tcodingg <princetlive@yahoo.com>
2022-02-18 13:09:02 -0500

    add results path to the router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home/Home";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Details from "./pages/Details/Details";
 import Search from "./components/Search/Search";
+import Results from "./pages/Results/Results";
 
 const App = () => {
    return (
@@ -10,6 +11,7 @@ const App = () => {
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/details/:id" element={<Details />} />
+            <Route path="/results" element={<Results />} />
          </Routes>
       </BrowserRouter>
    );
--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,1 +1,11 @@
 import "./Results.scss";
+
+const Results = () => {
+   return (
+      <section className="results">
+         <div className="results-container bd-container">results</div>
+      </section>
+   );
+};
+
+export default Results;
