07d6462b79e526fb1d1183b78b4085db8688d427
Tcodingg <princetlive@yahoo.com>
2022-12-31 16:48:39 -0500

    replace BrowserRouter by HashRouter. Add htaccess file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import Home from "./pages/Home/Home";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Details from "./pages/Details/Details";
-import Search from "./components/Search/Search";
-import Results from "./pages/Results/Results";
+import Home from './pages/Home/Home';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Details from './pages/Details/Details';
+import Search from './components/Search/Search';
+import Results from './pages/Results/Results';
 
 const App = () => {
-   return (
-      <BrowserRouter>
-         <Search />
-         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/results" element={<Results />} />
-         </Routes>
-      </BrowserRouter>
-   );
+  return (
+    <>
+      <Search />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/details/:id' element={<Details />} />
+        <Route path='/results' element={<Results />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.scss";
-import { localStore, persisterStore } from "./redux/store";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.scss';
+import { localStore, persisterStore } from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-   <Provider store={localStore}>
+  <HashRouter>
+    <Provider store={localStore}>
       <PersistGate persistor={persisterStore}>
-         <App />
+        <App />
       </PersistGate>
-   </Provider>,
-   document.getElementById("root")
+    </Provider>
+  </HashRouter>,
+  document.getElementById('root')
 );
